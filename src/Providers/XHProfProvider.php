00b23dcdc11f23f5943a36ccca74a0e9b4046368
emmanuil33 <evgenij.emm@gmail.com>
2020-09-28 12:08:03 +0300

    Update XHProfProvider.php

--- src/Providers/XHProfProvider.php
+++ src/Providers/XHProfProvider.php
@@ -27,7 +27,7 @@ class XHProfProvider implements ProviderInterface
     {
         $data = xhprof_disable();
         $runs = new XHProfRuns_Default(config('xhprof.output_dir', null));
-        $runs->save_run($data, config('xhprof.name'), str_replace('/', ':', request()->path()) . ':' . str_replace('.', '', microtime(true)));
+        $runs->save_run($data, config('xhprof.name'), str_replace('/', '_', request()->path()) . '_' . str_replace('.', '', microtime(true)));
     }
 
 }
